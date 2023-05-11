8fa3dd429583efeda4f723e9c547ab16b16ffcca
CTellezo <rt5960369@gmail.com>
2023-05-11 09:15:00 -0500

    HOY TE VENGO A CONTAR

--- 1EXM/app.js
+++ 1EXM/app.js
@@ -1,13 +1,13 @@
-let P = document.querySelector("V")
-let VP = document.querySelector("vp")
-let A = document.querySelector("MA")
-let VA = document.querySelector("va")
-let SUL = document.querySelector("RE")
+let P = document.querySelector("#V")
+let VP = document.querySelector("#vp")
+let A = document.querySelector("#MA")
+let VA = document.querySelector("#va")
+let SUL = document.querySelector("#RE")
 P.oninput=()=>{calcular()}
 A.oninput=()=>{calcular()}
 const calcular=()=>{
-    VP.innerHTML= `$(P.value) KG`
-    VA.innerHTML= `$(A.value) M`
+    VP.innerHTML= `${P.value} KG`
+    VA.innerHTML= `${A.value} M`
     SUL.innerHTML= `<img src="../IMG/C.jpg" class="w-25">`
     setTimeout(() => {
         let pe = parseInt(P.value);
@@ -20,11 +20,11 @@ const calcular=()=>{
         else if (IMC>=30&&IMC<35) {con="OBESIDAD I" } 
         else if (IMC>=35&&IMC<40) {con ="OBESIDAD II" }
         else{con="OBESIDAD III"}
-        SUL.innerHTML=<div class="text-center m-auto">
+        SUL.innerHTML=`<div class="text-center m-auto">
         <h1 class="text-danger">IMC</h1>
         <div class="card text-primary w-50 m-auto"><h2>$(IMC.toFixed(2))</h2></div>
         <h1 class="text-danger">CONDICION</h1>
         <div class="card text-primary w-50 m-auto"><h2>$(con)</h2></div>
-        </div>
+        </div>`
     } ,1200);
 }
